1cf4ba50163e8c9c9a3dbc19b9eda92b6baf7bff
Desetude <harry@harryfreeborough.com>
2017-12-17 16:40:27 +0000

    Textmode Cursor Positioning

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,7 +2,7 @@
 
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"
 void kernel_main(void) {
-    textmode_init(80, 25);
+    textmode_init(textmode_make_attrib(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK));
 
-    textmode_write_string("Hello!", textmode_make_attrib(VGA_COLOR_GREEN, VGA_COLOR_BLACK));
+    textmode_write_string("Hello!");
 }
