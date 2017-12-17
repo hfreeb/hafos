902f75c9a50cd802ad9ef3f1573da8f6476f2125
Harry Freeborough <harry@hfreeb.com>
2017-12-17 20:07:59 +0000

    GDT

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,10 @@
 #include <drivers/vga/textmode.h>
+#include <arch/i386/gdt.h>
 
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"
 void kernel_main(void) {
+    gdt_init();
+
     textmode_init(textmode_make_attrib(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK));
 
     textmode_write_string("Hello!");
