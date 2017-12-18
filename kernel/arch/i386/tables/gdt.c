c1996cfcfa5017fe3ad09ba47c93114b958a86a8
Harry Freeborough <harry@hfreeb.com>
2017-12-18 07:18:17 +0000

    Fixed GDT

--- kernel/arch/i386/tables/gdt.c
+++ kernel/arch/i386/tables/gdt.c
@@ -6,7 +6,7 @@
 struct gdt_pointer {
     uint16_t limit;
     uintptr_t base;
-};
+} __attribute__((packed));
 
 struct gdt_entry {
     uint16_t limit_low;
@@ -22,28 +22,27 @@ static struct gdt_entry gdt_entries[5];
 
 extern void gdt_flush(uintptr_t);
 
-static void gdt_set_entry(size_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t granularity) {
-    struct gdt_entry entry = gdt_entries[num];
-    entry.base_low = base & 0xFFFF;
-    entry.base_middle = (base >> 16) & 0xFF;
-    entry.base_high = (base >> 24) & 0xFF;
+static void gdt_set_gate(size_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran) {
+   gdt_entries[num].base_low = (base & 0xFFFF);
+   gdt_entries[num].base_middle = (base >> 16) & 0xFF;
+   gdt_entries[num].base_high = (base >> 24) & 0xFF;
 
-    entry.limit_low = limit & 0xFFFF;
-    entry.granularity = (limit >> 16) & 0x0F;
+   gdt_entries[num].limit_low = (limit & 0xFFFF);
+   gdt_entries[num].granularity = (limit >> 16) & 0x0F;
 
-    entry.granularity |= granularity & 0xF0;
-    entry.access = access;
+   gdt_entries[num].granularity |= gran & 0xF0;
+   gdt_entries[num].access = access;
 }
 
 void gdt_init(void) {
     gdt_ptr.limit = sizeof(gdt_entries) - 1;
-    gdt_ptr.base = (uintptr_t) &gdt_entries[0];
+    gdt_ptr.base = (uintptr_t) &gdt_entries;
 
-    gdt_set_entry(0, 0, 0, 0, 0);
-    gdt_set_entry(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); //Code
-    gdt_set_entry(2, 0, 0xFFFFFFFF, 0x92, 0xCF); //Data
-    gdt_set_entry(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); //User code
-    gdt_set_entry(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); //User data
+    gdt_set_gate(0, 0, 0, 0, 0);
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); //Code
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); //Data
+    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); //User code
+    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); //User data
 
     gdt_flush((uintptr_t) &gdt_ptr);
 }
