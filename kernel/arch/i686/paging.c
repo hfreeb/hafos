aea95e12761f3d0b92235c2a4481f8dc2f9a159a
Harry Freeborough <harry@hfreeb.com>
2018-06-10 09:08:10 +0100

    Install paging

---
+++ kernel/arch/i686/paging.c
@@ -0,0 +1,25 @@
+#include <hafos/paging.h>
+#include <stdint.h>
+
+extern void paging_load_directory(uint32_t *);
+extern void paging_enable(void);
+
+static uint32_t page_directory[1024] __attribute__((aligned(4096)));
+static uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+
+void paging_install(void) {
+    for (int i = 0; i < 1024; ++i) {
+        //SUPERVISOR | WRITE_ENABLED | NOT_PRESENT
+        page_directory[i] = 0x00000002;
+    }
+
+    for (int i = 0; i < 1024; ++i) {
+        //SUPERVISOR | WRITE_ENABLED | PRESENT
+        first_page_table[i] = (i * 0x1000) | 3;
+    }
+
+    page_directory[0] = ((uint32_t) first_page_table) | 3;
+
+    paging_load_directory(page_directory);
+    paging_enable();
+}
