2b85be0b1bbf612b1fa1754f15c94da0ba6dcabf
Harry Freeborough <harry@hfreeb.com>
2018-06-11 21:46:52 +0100

    Add mm.{h,c}

--- kernel/core/kernel.c
+++ kernel/core/kernel.c
@@ -4,7 +4,7 @@
 #include <drivers/keyboard.h>
 #include <stdio.h>
 #include <hafos/gdt.h>
-#include <hafos/paging.h>
+#include <hafos/mm.h>
 #include <hafos/idt.h>
 #include <hafos/irq.h>
 
@@ -12,37 +12,27 @@ extern uint32_t kernel_end;
 
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
-void kernel_main(multiboot_info_t *mbi, uint32_t mb_magic) {
-    gdt_install();
-    paging_install();
-    irq_init();
-    idt_init();
-
+void kernel_main(multiboot_info_t *mboot_header, uint32_t mboot_magic) {
     textmode_init(textmode_make_attrib(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK));
 
-    textmode_write_string("Hello, terminal!\n");
-
-    if (mbi->flags & (1 << 6)) {
-        multiboot_memory_map_t *mmap;
-
-        printf ("mmap_addr = 0x%X, mmap_length = 0x%X\n", (unsigned) mbi->mmap_addr, (unsigned) mbi->mmap_length);
-
-        for (mmap = (multiboot_memory_map_t *) mbi->mmap_addr;
-                (unsigned long) mmap < mbi->mmap_addr + mbi->mmap_length;
-                mmap = (multiboot_memory_map_t *) ((unsigned long) mmap + mmap->size + sizeof (mmap->size))) {
-
-            printf ("base_addr = 0x%08X, length = 0x%08X, type = 0x%X\n",
-                    (unsigned int) (mmap->addr & 0xFFFFFFFF),
-                    (unsigned int) (mmap->len & 0xFFFFFFFF),
-                    (unsigned int) mmap->type);
-        }
+    if (mboot_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        printf("Error: Not booted by a compliant bootloader.\n");
+        return;
     }
+
+    gdt_install();
+
+    if (!mm_install(mboot_header)) {
+        return;
+    }
+
+    irq_init();
+    idt_init();
 
     timer_install();
     keyboard_install();
 
     asm volatile ("sti");
-
 
     for (;;);
 }
