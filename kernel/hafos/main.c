fbe0de4a31e0398391794f0075f8895458454fbd
Harry Freeborough <harry@hfreeb.com>
2020-04-29 08:15:09 +0100

    Move textmode.{c,h} and io.{c,h} to a new io directory

--- kernel/hafos/main.c
+++ kernel/hafos/main.c
@@ -1,8 +1,8 @@
-#include "textmode.h"
+#include "io/textmode.h"
 
 void kernel_main(void)
 {
 	textmode_init(textmode_make_attrib(VGA_COLOR_GREEN, VGA_COLOR_BLACK));
- 
+
 	textmode_write_string("Hello, kernel World!\n");
 }
